57e931d3fea1e180dbd7c3187e548eff0729ea07
Starlone <starlone.passos@gmail.com>
2015-05-01 04:12:31 -0300

    Add deteccao de colisao. Add name aos GameObject.

--- js/windowgame.js
+++ js/windowgame.js
@@ -72,7 +72,7 @@ WindowGame.prototype.init = function(){
 function Scene(width,height){
     this.width = width;
     this.height = height;
-    this.camera = new GameObject(0,0,0,0);
+    this.camera = new GameObject('MainCamera',0,0,0,0);
     this.camX = 0;
     this.camY = 0;
     this.objs = [this.camera];
@@ -85,6 +85,9 @@ Scene.prototype.getHeight = function(){
 };
 Scene.prototype.getCamera = function(){
     return this.camera;
+};
+Scene.prototype.getObjs = function(){
+    return this.objs;
 };
 Scene.prototype.setSize = function(width,height){
     this.width = width;
@@ -116,11 +119,12 @@ Scene.prototype.check_move_cam = function(ctx){
         ctx.translate(x,y);
         this.camX = this.camera.getX();
         this.camY = this.camera.getY();
-	} 
+    } 
 }
 
 
-function GameObject(x,y,width,height){
+function GameObject(name, x, y, width, height){
+    this.name = name;
     this.x = x;
     this.y = y;
     this.width = width;
@@ -133,6 +137,9 @@ GameObject.prototype.getX = function(){
 };
 GameObject.prototype.getY = function(){
     return this.y;
+};
+GameObject.prototype.getColliders = function(){
+    return this.colliders;
 };
 GameObject.prototype.render = function(ctx){
     this.update();
@@ -157,7 +164,55 @@ GameObject.prototype.move = function(x,y){
     this.x = x;
     this.y = y;
 };
+GameObject.prototype.sum = function(x,y){
+    var xb = this.x;
+    var yb = this.y;
+    this.x += x;
+    this.y += y;
+    if(this.checkCollision()){
+        this.x = xb;
+        this.y = yb;        
+    }
+};
+GameObject.prototype.checkCollision = function(x,y){
+    var scene = this.parent;
+    var objs = scene.getObjs();    
+    for(var i in objs){
+        var obj = objs[i];        
+        if(this.name != obj.name){
+            if( checkColliders( this.getColliders(), obj.getColliders() ) )
+                return true;
+        }
+    }
+    return false;
+}
 
+function checkColliders(cols1, cols2){        
+    for(var i in cols1){
+        for(var j in cols2){
+            var col1 = cols1[i];                        
+            var col2 = cols2[j];
+            var ps1 = col1.getPoints();
+            var ps2 = col2.getPoints();
+            if(checkPolygonContido(ps1,ps2)){
+                return true;
+            }
+                        
+        }
+    }
+    return false;
+}
+
+function checkPolygonContido(ps1, ps2){
+    var min = ps2[0];
+    var max = ps2[3];
+    
+    for(var i in ps1){
+        if(ps1[i][0] >= min[0] && ps1[i][0] <= max[0] && ps1[i][1] >= min[1] && ps1[i][1] <= max[1])
+            return true;
+    }
+    return false;
+}
 
 function Joystick(){
     this.key = null;
@@ -179,11 +234,11 @@ Joystick.prototype.update = function(){
     if (key == '65'){ // Left
         this.x = -1;
     } else if (key == '68'){ // 'right'
-            this.x = 1;
+        this.x = 1;
     } else if (key == '87'){ // Up
-            this.y = -1;
+        this.y = -1;
     } else if (key == '83'){ // 'Down'
-            this.y = 1;
+        this.y = 1;
     }
 }
 
@@ -209,12 +264,10 @@ function ComponentPlayer(game,speed,gravity){
     this.speed = speed;
 };
 ComponentPlayer.prototype.update = function(obj){
-    var x = game.joystick.getAxis('horizontal');
-    var y = game.joystick.getAxis('vertical');
-    if(x)
-        obj.x += x * this.speed;
-    if(y)
-        obj.y += y * this.speed;
+    var x = game.joystick.getAxis('horizontal') * this.speed;
+    var y = game.joystick.getAxis('vertical') * this.speed;
+    if(x || y)
+        obj.sum(x,y);
 };
 
 
@@ -238,7 +291,7 @@ function ComponentRigidBody(gravity){
 }
 
 ComponentRigidBody.prototype.update = function(obj){
-    obj.y += this.gravity;
+    obj.sum(0, this.gravity);
 }
 
 function Collider(x1,y1,x2,y2){
@@ -248,7 +301,15 @@ function Collider(x1,y1,x2,y2){
     this.y2 = y2;
 }
 
-Collider.prototype.update = function(obj){
+Collider.prototype.getPoints = function(obj){
+    var pai = this.parent;
+    var lista = [];
         
+    lista.push([pai.x, pai.y]);
+    lista.push([pai.x + pai.width, pai.y]);
+    lista.push([pai.x, pai.y + pai.height]);
+    lista.push([pai.x + pai.width, pai.y + pai.height]);
+        
+    return lista;
 }
 
