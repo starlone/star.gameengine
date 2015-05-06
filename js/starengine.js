a1ce3f4166978d5e7d0ffc523e2d48f26f14f8cd
Starlone <starlone.passos@gmail.com>
2015-05-05 23:50:08 -0300

    add botoes

--- js/starengine.js
+++ js/starengine.js
@@ -275,7 +275,6 @@ se.Joystick = function (){
         self.reset();
     });
 }
-
 se.Joystick.prototype.update = function(){
     var key = this.key;
     if (key == '65'){ // Left
@@ -290,18 +289,29 @@ se.Joystick.prototype.update = function(){
         this.jump = true
     }
 }
-
 se.Joystick.prototype.reset = function (){
     this.x = 0;
     this.y = 0;
     this.jump = false;
 }
-
 se.Joystick.prototype.getAxis = function (name){
     if(name == 'horizontal') return this.x;
     if(name == 'vertical') return this.y;    
     if(name == 'jump') return this.jump;
     return 0;
+}
+se.Joystick.prototype.setAxis = function (name, val){
+    if(name == 'horizontal') 
+    	this.x = val;
+    if(name == 'vertical') 
+    	this.y = val;    
+    if(name == 'jump') 
+    	this.jump = val;
+}
+se.Joystick.prototype.resetAxis = function (name, val){
+    this.x = 0;
+    this.y = 0;    
+    this.jump = false;
 }
 
 
