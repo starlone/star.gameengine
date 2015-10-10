dcd42e13e2050e6795142aa85287784d7b249e27
Starlone <starlone.passos@gmail.com>
2015-10-10 01:30:44 -0300

    build.sh: add dev para ter arquivo completo

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
+
 closure=~/apps/compiler/compiler.jar;
 files=`find src -type f`;
-java -jar $closure --js $files --js_output_file starengine.min.js;
+fileout='starengine.min.js';
+case $1 in
+	'dev')
+		cat `find src -type f` > $fileout;
+		;;
+	*)
+		java -jar $closure --js $files --js_output_file $fileout;
+		;;
+esac
+
 
