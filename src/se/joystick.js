57114d5932c1e531a877a8809cbf08ee4e39a314
Starlone <starlone.passos@gmail.com>
2015-10-10 01:51:01 -0300

    Corrigindo bug de movimentacao com o teclado

--- src/se/joystick.js
+++ src/se/joystick.js
@@ -8,39 +8,20 @@ se.Joystick = function (){
     var y = 0;
     this.jump = false;
     $(document).keydown(function(e){
-        self.key = e.keyCode;
-        self.update();
+		self.keydown(e.keyCode);
     });
     $(document).keyup(function(e){
-        self.key = null;
-        self.reset();
+		self.keyup(e.keyCode);
     });
 }
-se.Joystick.prototype.update = function(){
-    var key = this.key;
-    if (key == '65'){ // Left
-        this.x = -1;
-    } else if (key == '68'){ // 'right'
-        this.x = 1;
-    } else if (key == '87'){ // Up
-        this.y = -1;
-    } else if (key == '83'){ // 'Down'
-        this.y = 1;
-    } else if (key == '32'){ // 'Space'
-        this.jump = true
-    }
-}
-se.Joystick.prototype.reset = function (){
-    this.x = 0;
-    this.y = 0;
-    this.jump = false;
-}
+
 se.Joystick.prototype.getAxis = function (name){
     if(name == 'horizontal') return this.x;
     if(name == 'vertical') return this.y;    
     if(name == 'jump') return this.jump;
     return 0;
 }
+
 se.Joystick.prototype.setAxis = function (name, val){
     if(name == 'horizontal') 
     	this.x = val;
@@ -49,9 +30,33 @@ se.Joystick.prototype.setAxis = function (name, val){
     if(name == 'jump') 
     	this.jump = val;
 }
-se.Joystick.prototype.resetAxis = function (name, val){
+
+se.Joystick.prototype.resetAxis = function (){
     this.x = 0;
     this.y = 0;    
     this.jump = false;
 }
 
+se.Joystick.prototype.keydown = function(key){
+    if (key == '65'){ // Left
+		this.setAxis('horizontal',-1);
+    } else if (key == '68'){ // 'right'
+		this.setAxis('horizontal',1);
+    } else if (key == '87'){ // Up
+		this.setAxis('vertical', -1);
+    } else if (key == '83'){ // 'Down'
+		this.setAxis('vertical', 1);
+    } else if (key == '32'){ // 'Space'
+		this.setAxis('jump', true);
+    }
+}
+
+se.Joystick.prototype.keyup = function(key){
+    if (key == '65' || key == '68'){ 
+		this.setAxis('horizontal',0);
+    } else if (key == '87' || key == '83'){
+		this.setAxis('vertical', 0);
+    } else if (key == '32'){
+		this.setAxis('jump', false);
+    }
+}
