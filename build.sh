84e88b81f9a43d9e89488964161963be28b1b2a2
Starlone <starlone.passos@gmail.com>
2015-11-16 23:54:49 -0200

    Adicionado parede no inicio. Chao aumentado. Add ajuda no script de build

--- build.sh
+++ build.sh
@@ -2,12 +2,34 @@
 closure=~/apps/compiler/compiler.jar;
 files=`find src -type f | tac`;
 fileout='starengine.min.js';
+help_msg="
+    Usage: ./build.sh [OPTION]\n
+	    -> min - Minifier\n
+	    -> dev - Developing build mode\n
+	    -> server - Start web server\n
+		-> help - Display this help\n
+    Example:
+	    ./build.sh min
+"
+function help(){
+    echo -e $help_msg;
+}
+
 case $1 in
+	'min')
+		java -jar $closure --js $files --js_output_file $fileout;
+		;;
 	'dev')
 		cat $files > $fileout;
 		;;
+	'server')
+		python -m SimpleHTTPServer;
+		;;
+	'help')
+		help;
+		;;
 	*)
-		java -jar $closure --js $files --js_output_file $fileout;
+		help;
 		;;
 esac
 
