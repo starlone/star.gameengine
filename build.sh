5cb7263c2cfb6e336030da439b6b7acb1035ae91
Starlone <starlone.passos@gmail.com>
2016-02-11 01:36:32 -0200

    build.sh - add opcao para fazer Download do closure compiler. Add help de devfg e bootstrap

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 closure=~/apps/compiler/compiler.jar;
 files=`find src -type f | tac`;
 fileout='starengine.min.js';
+url='https://dl.google.com/closure-compiler/compiler-latest.zip'
 help_msg="
     Usage: ./build.sh [OPTION]\n
+	    -> bootstrap - Download Closure Compiler\n
 	    -> min - Minifier\n
 	    -> dev - Developing build mode\n
 	    -> server - Start web server\n
+	    -> devfg - Developing build mode and start web server\n
 		-> help - Display this help\n
     Example:
 	    ./build.sh min
@@ -20,6 +23,13 @@ function concatfiles(){
 }
 
 case $1 in
+	'bootstrap')
+		mkdir -p ~/apps/compiler;
+		cd ~/apps/compiler;
+		wget $url;
+		unzip compiler-latest.zip;
+		rm compiler-latest.zip;
+		;;
 	'min')
 		java -jar $closure --js $files --js_output_file $fileout;
 		;;
