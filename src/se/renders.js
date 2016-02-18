e0831b3601139cc607c85d3eac3869ca35834121
Starlone <starlone.passos@gmail.com>
2016-02-18 00:56:24 -0200

    Add GrandientRenderer para render do background da scena

--- src/se/renders.js
+++ src/se/renders.js
@@ -34,3 +34,24 @@ se.ImageRenderer.prototype.render = function(ctx){
 se.ImageRenderer.prototype.setParent = function(obj){
     this.parent = obj;
 }
+
+/*
+   GradientRenderer
+*/
+se.GradientRenderer = function(color1, color2){
+    this.color1 = color1 || '#004CB3';
+    this.color2 = color2 || '#8ED6FF';
+}
+
+se.GradientRenderer.prototype.setParent = function(obj){
+    this.parent = obj;
+}
+
+se.GradientRenderer.prototype.render = function(ctx, params){
+    var grd = ctx.createLinearGradient(150, 0, 150, 300);
+    grd.addColorStop(0, this.color1);   
+    grd.addColorStop(1, this.color2);
+    ctx.fillStyle = grd;
+    ctx.fillRect(params.x, params.y, params.width, params.height);
+}
+
