a30909164d802ea9b2156fe054dec4da784a65e3
Starlone <starlone.passos@gmail.com>
2016-03-02 00:04:50 -0300

    Add RigidBodyRenderer que desenha o objeto de acordo com seu Body de Matter JS

--- src/se/renders.js
+++ src/se/renders.js
@@ -55,3 +55,50 @@ se.GradientRenderer.prototype.render = function(ctx, params){
     ctx.fillRect(params.x, params.y, params.width, params.height);
 }
 
+
+/*
+   RigidBodyRenderer - Based in Matter JS
+*/
+se.RigidBodyRenderer = function(color){
+    this.color = color;
+}
+
+se.RigidBodyRenderer.prototype.setParent = function(obj){
+    this.parent = obj;
+}
+
+se.RigidBodyRenderer.prototype.render = function(ctx){
+    var pos = this.parent.transform.position;
+    var part = this.parent.rigidbody.body;
+    var c = ctx;
+
+    // part polygon
+    c.beginPath();
+
+    for (var j = 1; j < part.vertices.length; j++) {
+        var x = part.vertices[j].x - pos.x;
+        var y = part.vertices[j].y - pos.y;
+        if (!part.vertices[j - 1].isInternal || showInternalEdges) {
+            c.lineTo(x, y);
+        } else {
+            c.moveTo(x, y);
+        }
+
+        if (part.vertices[j].isInternal && !showInternalEdges) {
+            x = part.vertices[(j + 1) % part.vertices.length].x - pos.x;
+            y = part.vertices[(j + 1) % part.vertices.length].y - pos.y;
+            c.moveTo(x, y);
+        }
+    }
+    
+    c.lineTo(part.vertices[0].x - pos.x, part.vertices[0].y - pos.y);
+    c.closePath();
+
+    c.fillStyle = this.color;
+    c.lineWidth = part.render.lineWidth;
+    c.strokeStyle = part.render.strokeStyle;
+    c.fill();
+
+    c.stroke();
+
+}
